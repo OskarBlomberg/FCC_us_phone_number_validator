2c1ecc96ad5730e2fa900a7107173093df9f4f03
Oskar Blomberg <blomberg.oskar@gmail.com>
2024-11-21 16:56:17 +0100

    finish js-file
    add functionality to check and clear buttons

--- script.js
+++ script.js
@@ -3,29 +3,28 @@ const outputEl = document.getElementById("results-div");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 
-const validateInput = (inputEl) => {
-  const acceptedValues = /1?[ ]?(\(?\d{3})( -)?(\d{3})( -)?(\d{4})/;
+const validateInputUS = (input) => {
+  const acceptedValues = /1?[ ]?(\(?\d{3})\)?( -)?(\d{3})( -)?(\d{4})/;
+  return acceptedValues.test(input.value.trim());
 };
 
+const renderOutput = (num) => {
+  outputEl.textContent = num
+    ? "Valid US number: " + inputEl.value
+    : "Invalid US number: " + inputEl.value;
+};
+
+const clearInput = (input) => (input.value = "");
+
 checkBtn.addEventListener("click", () => {
-  // validateInput();
-  // renderOutput();
-  // clearInput();
+  if (inputEl.value.trim()) {
+    renderOutput(validateInputUS(inputEl));
+    clearInput(inputEl);
+  } else {
+    alert("Please provide a phone number");
+  }
 });
 
-clearBtn.addEventListener("click", clearOutput());
+const clearOutput = () => (outputEl.innerHTML = "");
 
-/* 
-When you click on the #check-btn element without entering a value into the #user-input element, an alert should appear with the text "Please provide a phone number".
-When you click on the #clear-btn element, the content within the #results-div element should be removed.
-When the #user-input element contains 1 555-555-5555 and the #check-btn element is clicked, the #results-div element should contain the text "Valid US number: 1 555-555-5555".
-When the #user-input element contains 1 (555) 555-5555 and the #check-btn element is clicked, the #results-div element should contain the text "Valid US number: 1 (555) 555-5555".
-When the #user-input element contains 555-5555 and the #check-btn element is clicked, the #results-div element should contain the text "Invalid US number: 555-5555". */
-
-/* 1 555-555-5555
-1 (555) 555-5555
-1(555)555-5555
-1 555 555 5555
-5555555555
-555-555-5555
-(555)555-5555 */
+clearBtn.addEventListener("click", clearOutput);
